9e67bb0b350d8e7fa8920bab7f0a4e4b657976ed
ArnavMohan18 <arnanmohan18@gmail.com>
2026-05-27 12:25:19 -0700

    split the sensor collection to different files

---
+++ ESP32C3/vitals_sensor_module/sensors.h
@@ -0,0 +1,26 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+
+#include <Arduino.h>
+
+struct SensorPacket {
+
+  int32_t heartRate;
+  int32_t spo2;
+
+  float temperature;
+
+  float ax;
+  float ay;
+  float az;
+
+  float gx;
+  float gy;
+  float gz;
+};
+
+bool initSensors();
+
+bool readSensors(SensorPacket &data);
+
+#endif
